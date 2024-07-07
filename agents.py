04d39cc1ba4ba8f1263c53ef4cc133b0314675d5
ishmam97 <ishmam.a.solaiman@gmail.com>
2024-07-07 03:27:41 -0500

    add multiple model options

--- agents.py
+++ agents.py
@@ -3,11 +3,23 @@ from textwrap import dedent
 from langchain.chat_models import ChatOpenAI
 
 class CustomAgents():
+    LLAMA3 = ChatOpenAI(
+        model="llama3",
+        base_url="http://localhost:11434/v1",
+        api_key="NA"  # Make sure this is correct
+    )
+
+    mistral = ChatOpenAI(
+        model="mistral-openorca",
+        base_url="http://localhost:11434/v1",
+        api_key="NA"  # Make sure this is correct
+    )
 
     gpt35 = ChatOpenAI(
         temperature = 0.9,
         model_name = "gpt-3.5-turbo-0125",
     )
+    mainModel = LLAMA3
 
     def story_writer(self):
         return Agent(
@@ -19,7 +31,7 @@ class CustomAgents():
                 Ensure the story is well written and is friendly for children under 13. The story should have a good moral lesson and not be too short.
                 You can add a little fantasy to the story and use example plot points from popular stories.
             """),
-            llm = self.gpt35,
+            llm = self.mainModel,
             # Add tools if necessary, e.g., tools=[tool_1, tool_2],
             allow_delegation=False,
             verbose=True,
@@ -35,7 +47,7 @@ class CustomAgents():
             goal=dedent("""
                 Describe the characters in the story with rich details.
             """),
-            llm = self.gpt35,
+            llm = self.mainModel,
             # Add tools if necessary, e.g., tools=[tool_1, tool_2],
             verbose=True,
             allow_delegation=False,
